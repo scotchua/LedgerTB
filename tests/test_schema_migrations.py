527f58499fc35128b8ed2cd120ffe9d0820a408f
scotchua <scott.edwards@scotchua.com>
2026-08-28 23:04:36 -0700

    Update migration-list assertion for 029_payroll_recording

--- tests/test_schema_migrations.py
+++ tests/test_schema_migrations.py
@@ -37,7 +37,8 @@ def test_create_tables_records_migrations(db):
         "020_book_audit_events", "021_import_batch_reversal",
         "022_account_grouping", "023_cash_flow_section",
         "024_document_audits", "025_inventory", "026_bank_connections",
-        "027_bank_connection_syncs", "028_fixed_assets"]
+        "027_bank_connection_syncs", "028_fixed_assets",
+        "029_payroll_recording"]
     conn.close()
 
 
@@ -50,7 +51,7 @@ def test_create_tables_is_idempotent(db):
 
     cur = conn.cursor()
     cur.execute("SELECT COUNT(*) FROM schema_migrations")
-    assert cur.fetchone()[0] == 28
+    assert cur.fetchone()[0] == 29
     conn.close()
 
 
