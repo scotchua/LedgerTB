c3dc895edfde3c6c4e03c222260510bebc3440a4
scotchua <scott.edwards@scotchua.com>
2026-08-30 09:26:44 -0700

    Update migration-list assertion for 038

--- tests/test_schema_migrations.py
+++ tests/test_schema_migrations.py
@@ -42,7 +42,7 @@ def test_create_tables_records_migrations(db):
         "031_vendor_created_at", "032_ar_ap",
         "033_depreciation_draft_links", "034_bank_feed_remote_accounts",
         "035_shared_domain_contracts", "036_ar_ap_allocations",
-        "037_payroll_import_staging"]
+        "037_payroll_import_staging", "038_invoice_inventory"]
     conn.close()
 
 
@@ -55,7 +55,7 @@ def test_create_tables_is_idempotent(db):
 
     cur = conn.cursor()
     cur.execute("SELECT COUNT(*) FROM schema_migrations")
-    assert cur.fetchone()[0] == 37
+    assert cur.fetchone()[0] == 38
     conn.close()
 
 
